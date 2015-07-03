f465f562f5d489c7d00b6594b209af12108b1097
Antonio Cruz <tonchmx@hotmail.com>
2015-07-03 09:17:18 -0500

    Catalogo basico.

--- modules/catalogo/config/catalogo.routes.js
+++ modules/catalogo/config/catalogo.routes.js
@@ -4,8 +4,12 @@ angular.module('catalogo').config(['$routeProvider',
             .when('/', {
                 templateUrl: '/modules/catalogo/views/catalogo.view.html'
             })
+            .when('/carrito', {
+                templateUrl: '/modules/catalogo/views/carrito.view.html'
+            })
             .when('/:sku', {
                 templateUrl: '/modules/catalogo/views/producto.view.html'
             })
+            .otherwise('/')
     }
 ]);
