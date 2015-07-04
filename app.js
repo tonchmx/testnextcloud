f74ca050680206415066a6975b8f977b0e01d887
Antonio Cruz <tonchmx@hotmail.com>
2015-07-04 16:59:38 -0500

    Carrito y uso de localstorage.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var mainApplicationModuleName = 'nextcloud';
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'ngResource', 'slick', 'catalogo']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'ngResource', 'slick', 'LocalStorageModule', 'catalogo']);
+
+mainApplicationModule
+    .config(['localStorageServiceProvider',
+        function(localStorageServiceProvider){
+            localStorageServiceProvider
+                .setPrefix('nextcloud');
+        }
+    ]);
 
 angular.element(document).ready(function(){
     angular.bootstrap(document, [mainApplicationModuleName])
--- modules/catalogo/controllers/catalogo.controller.js
+++ modules/catalogo/controllers/catalogo.controller.js
@@ -1,15 +1,8 @@
-angular.module('catalogo').controller('CatalogoController', ['$scope', '$sce', '$routeParams', 'Catalogo',
-    function($scope, $sce, $routeParams, Catalogo) {
+angular.module('catalogo').controller('CatalogoController', ['$scope', '$sce', '$routeParams', 'Catalogo', 'localStorageService',
+    function($scope, $sce, $routeParams, Catalogo, localStorageService) {
 
         $scope.predicate = 'fecha_alta';
         $scope.reverse = true;
-
-        $scope.sizes = [ {code: 1, name: 'n1'}, {code: 2, name: 'n2'}];
-        $scope.update = function() {
-            console.log($scope.item.code, $scope.item.name)
-        };
-
-
         $scope.orders = [
             {predicate: 'fecha_alta', reverse:true, name: 'Ordenar por: Más recientes'},
             {predicate: 'sku', reverse:false, name: 'Ordenar por: Alfabéticamente A-Z'},
@@ -17,6 +10,8 @@ angular.module('catalogo').controller('CatalogoController', ['$scope', '$sce', '
             {predicate: 'price', reverse:false, name: 'Ordenar por: Menor a Mayor Precio'},
             {predicate: 'price', reverse:true, name: 'Ordenar por: Mayor a Menor Precio'}
         ];
+
+        $scope.total = 0;
 
         $scope.cambiarOrden = function(){
             $scope.predicate = $scope.orden.predicate;
@@ -36,6 +31,56 @@ angular.module('catalogo').controller('CatalogoController', ['$scope', '$sce', '
                     $scope.producto.review = $sce.trustAsHtml($scope.producto.review);
                 }
             });
+        };
+
+        $scope.agregarAlCarrito = function(sku){
+            var formCantidad = this.cantidad;
+            this.cantidad = null;
+            if(formCantidad == null || formCantidad == 0){
+                formCantidad = 1;
+            }
+
+            console.log(formCantidad);
+            var producto = Catalogo.get({sku: sku}, function(){
+                var carrolocal = localStorageService.get('carrito');
+                if(carrolocal){
+                    if(carrolocal[sku]){
+                        carrolocal[sku].cantidad += formCantidad;
+                    } else {
+                        carrolocal[sku] = {
+                            'sku': producto.sku,
+                            'precio': producto.price,
+                            'cantidad': formCantidad
+                        };
+                    }
+
+                } else {
+                    var carro = {};
+                    carro[sku] = {
+                        'sku': producto.sku,
+                        'precio': producto.price,
+                        'cantidad': formCantidad
+                    };
+                    carrolocal = carro;
+                }
+
+                return localStorageService.set('carrito', carrolocal);
+            });
+        };
+
+        $scope.getCarrito = function(){
+            var carrito = localStorageService.get('carrito');
+            $scope.carrito = carrito;
+            var size = Object.size(carrito);
+            console.log(size);
+        };
+
+        Object.size = function(obj) {
+            var size = 0, key;
+            for(key in obj){
+                if(obj.hasOwnProperty(key)) size++;
+            }
+            return size;
         }
 
     }
